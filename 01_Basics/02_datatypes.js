16c4b97564c21f92ee09f38026d1f24e4fa1cabc
Harsh Dhoriyani <harshdhoriyani2005@gmail.com>
2025-04-02 23:25:08 +0530

    Add files via upload

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";  // treats all JS code as newer version
+
+// alert("hello") we are using node js and not browser
+
+let name = "harsh" // string
+let age = 19  // integer
+let isLoggedIn = false // boolean
+
+// Data Types:
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined 
+// symbol => unique
+
+console.log(typeof null);  // type of null is object.
+console.log(typeof undefined); // type of undefined is undefined.
---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,41 @@
+let score = "33abc"
+
+// console.log(typeof score);
+
+let valueinNumber = Number(score)
+
+// console.log(typeof valueinNumber);
+
+// console.log(valueinNumber);
+
+// Conversion to Number:
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+
+// Conversion to Boolean:
+// 1 => true
+// 0 => false
+// "" => false
+// "harsh" => true
+
+// ********** Operations **********
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " harsh" 
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);   // 122 
+// console.log(1 + 2 + "2");   // 32
+
+
+
