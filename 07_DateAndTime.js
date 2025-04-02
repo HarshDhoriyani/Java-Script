00cc0e010234d54e6450323d4fea992910f9db5b
Harsh Dhoriyani <harshdhoriyani2005@gmail.com>
2025-04-02 19:38:17 +0530

    Add files via upload

---
+++ 07_DateAndTime.js
@@ -0,0 +1,21 @@
+// Dates
+
+let mydate = new Date()
+// console.log(typeof mydate);
+
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toLocaleString());
+
+// in JS, months start with 0 meaning 0 -> Jan, 1 -> Feb and so on...
+let date = new Date(2025, 2, 23)
+// console.log(date.toDateString());
+
+
+let createdate = new Date("2025-02-23")
+// console.log(createdate.toLocaleString());
+ 
+let mytimestamp = Date.now()
+console.log(mytimestamp);
+
