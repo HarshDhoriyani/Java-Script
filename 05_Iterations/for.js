48f9885304f98be7004e6f95da0f7b4cc41d010c
Harsh Dhoriyani <harshdhoriyani2005@gmail.com>
2025-06-02 10:07:22 +0000

    For loop learnt.

---
+++ 05_Iterations/for.js
@@ -0,0 +1,23 @@
+// for
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// for (let index = 0; index < 5; index++) {
+//     const element = index;
+//     console.log(element);
+// }
+
+// for (let i = 0; i < 5 ; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//     for (let j = 0; j < 5; j++) {
+//         console.log(`Inner loop value: ${j} and inner loop ${i}`);
+//     }
+// }
+
+let myArray = ["flash", "batman", "superman"]
+for (let i = 0; i < myArray.length ; i++) {
+    console.log(myArray[i]);
+}
